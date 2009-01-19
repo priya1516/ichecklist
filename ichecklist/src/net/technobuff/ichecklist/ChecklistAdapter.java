726dbbdea7ecd2040e6c18c1e041af4fa33188e9
kavitanpatil <kavitanpatil@d2fb747c-d8ce-11dd-8179-53c65a4d6d95>
2009-01-19 18:41:01 +0000

    Corrected labels.

--- ichecklist/src/net/technobuff/ichecklist/ChecklistAdapter.java
+++ ichecklist/src/net/technobuff/ichecklist/ChecklistAdapter.java
@@ -19,11 +19,16 @@
 package net.technobuff.ichecklist;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 /**
  * The checklist adapter.
@@ -31,13 +36,28 @@ import android.widget.SimpleCursorAdapter;
  * @author Kavita
  */
 public class ChecklistAdapter extends SimpleCursorAdapter {
+  private TextView text;
+  private Context context;
+  private Cursor cursor;
+  
   public ChecklistAdapter(Activity context, int task_row, Cursor tasks2, String[] from, int[] to) { 
     super(context , task_row,tasks2, from, to); 
+    this.cursor = cursor;
+    this.context = context;
   } 
 
   public View getView(int position, View convertView, ViewGroup parent) { 
     View row = super.getView(position, convertView, parent); 
-
+    TextView text = (TextView)row.findViewById(R.id.checklist_name_text);
+    text.setTextColor(new ColorStateList(
+        new int[][] { 
+            new int[] { android.R.attr.state_selected }, 
+            new int[0], 
+        }, new int[] { 
+            Color.parseColor("#D20D2A"), 
+            Color.BLACK, 
+        } 
+));    
     if(position % 2 == 0) {
       row.setBackgroundColor (Color.rgb(170, 172, 240) ); 
     } 
